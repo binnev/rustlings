26896fe54fee29fc9379335f6b26745cd744cd14
binnev <byntje@gmail.com>
2023-12-05 07:52:32 +0100

    finished functions

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -3,11 +3,11 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+
+    // OK so I can discard / override a variable's type by doing another 'let'
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
