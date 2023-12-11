93639db163277bf8b7a5385050566571dfec3286
binnev <byntje@gmail.com>
2023-12-11 22:00:37 +0100

    move semantics

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,14 +6,19 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
     let mut x = 100;
-    let y = &mut x;
-    let z = &mut x;
-    *y += 100;
-    *z += 1000;
+    // normally, multiple mutable borrows would not be allowed. But if we use
+    // scopes, the first mutable borrow goes out of scope before we make the
+    // second one.
+    {
+        let y = &mut x;
+        *y += 100;
+    }
+    {
+        let z = &mut x;
+        *z += 1000;
+    }
     assert_eq!(x, 1200);
 }
