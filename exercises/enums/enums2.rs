5106b271da8c44acab607f8233784e2a9c38ba7f
binnev <byntje@gmail.com>
2023-12-24 12:50:51 +0100

    did enums

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -3,11 +3,12 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move { x: i64, y: i64 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
@@ -18,10 +19,10 @@ impl Message {
 
 fn main() {
     let messages = [
+        Message::Quit,
         Message::Move { x: 10, y: 30 },
         Message::Echo(String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
-        Message::Quit,
     ];
 
     for message in &messages {
