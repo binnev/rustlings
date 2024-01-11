418153e7787c0fcbf3b1117f1ae8efd37b6dc658
binnev <byntje@gmail.com>
2024-01-11 17:50:47 +0100

    quiz 3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,23 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt::Display;
 
-pub struct ReportCard {
-    pub grade: f32,
+// Here I declare that ReportCard has some generic type contents.
+pub struct ReportCard<D: Display> {
+    pub grade: D,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+// Here I declare that this implementation too is only valid for ReportCards
+// containing Display types.
+impl<D: Display> ReportCard<D> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -37,6 +42,9 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
+        // I was expecting to have to declare the contents of ReportCard here --
+        // i.e. do ReportCard::<f32> {...}. But apparently the compiler can
+        // infer that the f32 type implements Display.
         let report_card = ReportCard {
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
@@ -50,9 +58,10 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
+        // Ditto here, the compiler infers that &str implements Display and is
+        // thus a valid contained type
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
