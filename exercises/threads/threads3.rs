1627fa384d7fe9fd952b93732420891f49ed1822
binnev <byntje@gmail.com>
2024-01-28 11:04:54 +0100

    threads

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -31,19 +29,25 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
 
+    // I think here .clone() just gives us a new _pointer_ to tx that we can
+    // move into each thread closure.
+    let tx1 = tx.clone();
+    let tx2 = tx.clone();
+
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+            tx1.send(*val).unwrap();
+            // which asshole decided from_seconds(1) was a good idea...
+            thread::sleep(Duration::from_millis(10));
         }
     });
 
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+            tx2.send(*val).unwrap();
+            thread::sleep(Duration::from_millis(10));
         }
     });
 }
